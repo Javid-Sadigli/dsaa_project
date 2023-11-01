0a51e74094f312be5dba3efd8903737fd538cce5
Javid Sadigli <cavidoffical@gmail.com>
2023-11-01 15:15:44 +0400

    addelector

--- project.c
+++ project.c
@@ -17,6 +17,7 @@ T_Elector creationelector();
 T_Elector creation_T_Elector_Linked_List();
 T_Elector insertSorted(T_Elector head, T_Elector newVoter);
 void displaylist(T_Elector head);
+void addelector(T_Elector * ptr_to_head, char name[], long cin_num, int choice);
 
 int alphaOrder(const char *name1, const char *name2);
 
@@ -24,9 +25,20 @@ int main(int argc, char const *argv[])
 {
     T_Elector head = creation_T_Elector_Linked_List();
     displaylist(head);
-    
+    addelector(&head, "John", 212, 2);
+    displaylist(head);
     return 0;
 }
+
+void addelector(T_Elector * ptr_to_head, char name[], long cin_num, int choice)
+{
+    T_Elector newVoter = (T_Elector)malloc(sizeof(elector));
+    strcpy(newVoter->name, name);
+    newVoter->choice = choice;
+    newVoter->cin_num = cin_num; 
+    *ptr_to_head = insertSorted(*ptr_to_head, newVoter);
+}
+
 void displaylist(T_Elector head)
 {
     printf("\nHead -> ");
