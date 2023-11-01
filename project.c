86d5336a7768117c62910dae769e2d5117eb69fa
Javid Sadigli <cavidoffical@gmail.com>
2023-11-01 14:23:30 +0400

    sort

--- project.c
+++ project.c
@@ -1,24 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-# define MAX_NAME_SIZE 100
-typedef struct
+#include <string.h>
+
+#define MAX_NAME_SIZE 100
+typedef struct elector
 {
     char name[MAX_NAME_SIZE];
     long cin_num;
     int choice;
-    struct elector * next;
-}elector;
+    struct elector *next;
+} elector;
 
-typedef elector* T_Elector;
+typedef elector *T_Elector;
 
 T_Elector creationelector();
 T_Elector creation_T_Elector_Linked_List();
+T_Elector insertSorted(T_Elector head, T_Elector newVoter);
 void displaylist(T_Elector head);
+
+int alphaOrder(const char *name1, const char *name2);
 
 int main(int argc, char const *argv[])
 {
-    T_Elector head= creation_T_Elector_Linked_List();
+    T_Elector head = creation_T_Elector_Linked_List();
     displaylist(head);
+    
     return 0;
 }
 void displaylist(T_Elector head)
@@ -34,29 +40,50 @@ void displaylist(T_Elector head)
 
 T_Elector creationelector()
 {
-    T_Elector voter = (T_Elector)malloc(sizeof(elector));
-    printf("\nEnter the new voter's name : "); 
-    scanf("%s", &(voter->name));
-    printf("Enter the new voter's ID number : "); 
+    T_Elector voter = (T_Elector) malloc(sizeof(elector));
+    printf("\nEnter the new voter's name: ");
+    scanf("%s", voter->name);
+    printf("Enter the new voter's ID number: ");
     scanf("%ld", &(voter->cin_num));
-    printf("Enter the new voter's choice : "); 
+    printf("Enter the new voter's choice: ");
     scanf("%d", &(voter->choice));
     voter->next = NULL;
-    return voter; 
+    return voter;
 }
 
 T_Elector creation_T_Elector_Linked_List()
 {
     T_Elector head = NULL;
-    
-    int number_of_voters; 
-    printf("\nEnter the number of voters : "); scanf("%d", &number_of_voters);
-    T_Elector voter = creationelector();
-    head = voter;
-    for (int i = 1; i < number_of_voters; i++)
+    T_Elector newVoter;
+    int number_of_voters;
+    printf("\nEnter the number of voters: ");
+    scanf("%d", &number_of_voters);
+    for (int i = 0; i < number_of_voters; i++)
     {
-        voter->next = creationelector();
-        voter = voter->next;
+        newVoter = creationelector();
+        head = insertSorted(head, newVoter);
     }
     return head;
+}
+
+T_Elector insertSorted(T_Elector head, T_Elector newVoter)
+{
+    if (head == NULL || alphaOrder(newVoter->name, head->name) < 0)
+    {
+        newVoter->next = head;
+        return newVoter;
+    }
+    T_Elector current = head;
+    while (current->next != NULL && alphaOrder(newVoter->name, current->next->name) >= 0)
+    {
+        current = current->next;
+    }
+    newVoter->next = current->next;
+    current->next = newVoter;
+    return head;
+}
+
+int alphaOrder(const char *name1, const char *name2)
+{
+    return strcmp(name1, name2);
 }
