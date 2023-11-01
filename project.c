17d4fc989143b98e6a1846335e446d93c937b560
Javid Sadigli <cavidoffical@gmail.com>
2023-11-01 22:40:04 +0400

    MyMessage

--- project.c
+++ project.c
@@ -13,26 +13,17 @@ typedef struct elector
 
 typedef elector *T_Elector;
 
+// Function prototypes
 T_Elector creationelector();
-
 T_Elector creation_T_Elector_Linked_List();
-
 T_Elector insertSorted(T_Elector head, T_Elector newVoter);
-
 void displaylist(T_Elector head);
-
 void addelector(T_Elector *ptr_to_head, char name[], long cin_num, int choice);
-
 int alphaOrder(const char *name1, const char *name2);
-
 void decomposelist(T_Elector head, T_Elector *headLeft, T_Elector *headRight, T_Elector *headWhite);
-
 int countelector(T_Elector head);
-
 int findelector(T_Elector head, long cin_num);
-
 void deletelector(T_Elector *head, long cin_num);
-
 void sortlist(T_Elector head);
 
 int main(int argc, char const *argv[])
